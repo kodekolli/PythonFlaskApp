e274abe9cc2ddc9d78be48d5bc687835ece88e81
RevanthTiruveedhi <78875340+RevanthTiruveedhi@users.noreply.github.com>
2021-03-06 23:07:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ Flask App
 '''
 from flask import Flask
 from flask_restful import Api  # pragma: no cover
-from handlers.routes import configure_routes
-from handlers.api import sampleAPI  # pragma: no cover
+from Python_Flask_App.handlers.routes import configure_routes
+from Python_Flask_App.handlers.api import sampleAPI  # pragma: no cover
 
 app = Flask(__name__)
 api = Api(app)
